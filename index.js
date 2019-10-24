9236823b19dc48aa75e6209b7841b5421edf293b
Vladimir Goncharenko <df.201640@yahoo.com>
2019-10-24 23:57:00 +0300

    Add file app.js with server

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-    res.status(200).json({
-        message: 'Working'
-    });
-});
-app.listen(5000, () => {
-    console.log('Server has been started');
+const app = require('./app');
+const port = process.env.PORT || 5000;
+app.listen(5000, () => { console.log(`Server has been started on  ${port}`);
 });
