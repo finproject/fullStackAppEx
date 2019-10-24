a3f44933f383722d2c1e431532102e064255bf11
Vladimir Goncharenko <df.201640@yahoo.com>
2019-10-25 01:29:08 +0300

    Added functionality in controllers and routes

--- routes/auth.js
+++ routes/auth.js
@@ -2,10 +2,8 @@ const express = require('express');
 const controller = require('../controllers/auth');
 const router = express.Router();
 // localhost:5000/api/auth/login
-router.get('/login', controller.login);
+router.post('/login', controller.login);
 // localhost:5000/api/auth/register
-router.get('/register', controller.register);
-
-
+router.post('/register', controller.register);
 
 module.exports = router;
