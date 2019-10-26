ea6dcf0a277de89680549d82e068492e53056273
Vladimir Goncharenko <df.201640@yahoo.com>
2019-10-27 00:36:34 +0300

    Added function: validation of email and create and save user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,19 +1,35 @@
 const User = require('../models/User');
 
 module.exports.login = function (req, res) {
-   res.status(200).json({
-       login: {
-           email: req.body.email,
-           password: req.body.password
-       }
-   });
+    res.status(200).json({
+        login: {
+            email: req.body.email,
+            password: req.body.password
+        }
+    });
 };
 
-module.exports.register = function (req,res) {
-    const user = new User({
-       email:req.body.email,
-       password: req.body.password
-    });
-    user.save().then(()=> console.log('User created'));
+module.exports.register = async function (req, res) {
+    const candidate = await User.findOne({email: req.body.email});
+    if (candidate) {
+        //The user exists, need to send an error
+        res.status(409).json({
+            message: 'This email is already exists'
+        });
+    } else {
+        //Need to create a user
+        const user = new User({
+            email: req.body.email,
+            password: req.body.password
+        });
+
+        try {
+            await user.save();
+            res.status(201).json(user);
+        } catch (e) {
+            // Error handling
+        }
+    }
+
 }
 
