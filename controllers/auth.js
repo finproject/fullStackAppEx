b99a41eb0964c125c420b5e88b37939048ad2e27
Vladimir Goncharenko <df.201640@yahoo.com>
2019-10-27 14:12:29 +0200

    Add module error handler

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const keys = require('../config/keys');
+const errorHandler = require('../utils/errorHandler');
 
 module.exports.login = async function (req, res) {
     const candidate = await User.findOne({email: req.body.email})
@@ -49,7 +50,7 @@ module.exports.register = async function (req, res) {
             await user.save();
             res.status(201).json(user);
         } catch (e) {
-            // Error handling
+            errorHandler(res, e);
         }
     }
 }
